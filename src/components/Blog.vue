822be0692ddc096af32adf9c3d272c3ad0881874
robertoAureli <r.aureli@protonmail.com>
2021-10-03 11:31:05 +0200

    Effetto card corso

---
+++ src/components/Blog.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="blog">
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Blog',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
