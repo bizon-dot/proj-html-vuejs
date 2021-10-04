bdbc004f4200ff910dfa5863904cf8c3dccad105
robertoAureli <r.aureli@protonmail.com>
2021-10-04 12:06:07 +0200

    Sezione Newsletter

--- src/components/ContactMe.vue
+++ src/components/ContactMe.vue
@@ -1,19 +1,28 @@
 <template>
 <div class="contact">
-  <div class="container">
+  <!-- <div class="container"> -->
     <div class="row">
       <div class="col">
-          <h2>Newsletter to get in touch </h2>
+         <img class="photo-right" src="../assets/img/artist-shape-color-paint-top-left-300x170.png"  />
+      </div>
+    
+      <div class="col">
+          <h2 class="title">Newsletter to get in touch </h2>
       </div>
       <div class="col">
         <input type="email" id="email" placeholder="Your email"
        pattern=".+@globex\.com" size="30" required>
 
       </div>
+      <div class="col" >
+         <img src="../assets/img/artist-shape-color-paint-bottom-right-300x153.png"  />
+      </div>
     </div>
+      
   
+
   </div>
-  </div>
+  
 </template>
 
 <script>
@@ -28,21 +37,44 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
   @import "./style/general.scss";
+  .photo-right{
+    height: 64%;
+    width: auto;
+  }
 
 .row{
    display: flex;
    flex-direction: row;
+   //justify-content: flex-start;
    
    .col{
-     line-height: 200px;  
+     line-height: 153px;  
      text-align: center;
-     flex: 50%;
+     flex: 25%;
+
+     .title{
+       color: white;
+       font-size: 25px;
+       font-weight: bolder;
+     }
+
+     input[type=email] {
+        border: none;
+        border-bottom: 2px solid white;
+        outline: 0 none;
+        background-color: $dark-vibrant;
+        
+      }
+      ::placeholder{
+          color:white;
+          font-size:20px;
+        }
    }
 }
 
 .contact{
   background-color: $dark-vibrant;
-  min-height: 200px;
+  max-height: 153px;
 }
 
 </style>
